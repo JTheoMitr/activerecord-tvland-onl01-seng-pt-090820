8722135ca9e898d9f61acfd2947295b81b5f8761
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-12-03 17:00:46 -0600

    commit tree f69ee7fc55667be47aefbf57bbec5d2be33d467f

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
 class Show < ActiveRecord::Base
-  
+
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        all_actors = []
+        self.characters.each do |c|
+        id = c.actor_id
+        actor = Actor.find(id)
+        actor_name = actor.first_name + " " + actor.last_name
+        all_actors << actor_name
+        end
+        all_actors
+    end
+
 end
